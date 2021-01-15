e36cebb64a7b76c34165db79fc0af47851bcad08
KESSIR Ouassim <wassim.kes213@gmail.com>
2021-01-15 23:52:31 +0100

    updated project

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require('dotenv').config()
 const {Client} = require('discord.js')
-const client  = Client();
+const bot  = new Client()
+
+bot.on('ready',()=>{
+    console.log(`${bot.user.tag} has logged in`)
+})
+
+bot.on('message',(msg)=>{
+    msg.reply('this is a reply')
+    .then(()=>{
+        console.log('message has been sent')
+    })
+    .catch(err => console.log(err))
+})
+
+bot.login(process.env.discord_token)
 
